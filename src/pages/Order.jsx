f7607c22de7b00466f56a800401c48687606fb8c
kofimuad <iamkofiagyei@gmail.com>
2026-09-17 21:34:36 +0000

    updated list

--- src/pages/Order.jsx
+++ src/pages/Order.jsx
@@ -5,7 +5,7 @@ import useMenu from '../hooks/useMenu'
 import useActiveCycle from '../hooks/useActiveCycle'
 import { apiUrl } from '../lib/api'
 
-const BRANCHES    = ['Army', 'Navy', 'Air Force', 'Marines', 'Coast Guard', 'Space Force', 'Lackland']
+const BRANCHES    = ['Army', 'Marines', 'Coast Guard', 'Space Force', 'Lackland Gate', 'Lackland Shoppette', 'BAMC']
 const STORAGE_KEY = 'obaa_yaa_customer_info'
 
 function loadSavedInfo() {
